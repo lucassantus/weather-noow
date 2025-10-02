d2489a1f4ceccb09ea3a1f33402f8eb607781f3e
Lucas Santos <leos.developer@gmail.com>
2025-10-02 18:31:08 -0300

    chore: add biome linter

--- src/app/weather/[locationKey]/(default)/components/header.tsx
+++ src/app/weather/[locationKey]/(default)/components/header.tsx
@@ -13,23 +13,21 @@ export function Header({ className }: HeaderProps) {
   const router = useRouter();
 
   return (
-   
-      <div
-        className={cn(
-          "flex h-16 w-full items-center justify-between rounded-lg border border-custom-gray-500 bg-custom-gray-200/40 bg-custom-gray-700 p-6 shadow-xs",
-          className,
-        )}
+    <div
+      className={cn(
+        "flex h-16 w-full items-center justify-between rounded-lg border border-custom-gray-500 bg-custom-gray-200/40 p-6 shadow-xs",
+        className,
+      )}
+    >
+      <Button
+        className="flex items-center gap-2 text-xl text-white/60 no-underline!"
+        onClick={() => router.back()}
+        aria-label="Retornar para a página anterior"
+        variant="link"
       >
-        <Button
-          className="flex items-center gap-2 text-xl text-white/60 no-underline!"
-          onClick={() => router.back()}
-          aria-label="Retornar para a página anterior"
-          variant="link"
-        >
-          <LogoIcon className="h-7 w-7" />
-          Weather Noow
-        </Button>
-      </div>
-   
+        <LogoIcon className="h-7 w-7" />
+        Weather Noow
+      </Button>
+    </div>
   );
 }
